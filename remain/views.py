821803d00d01e070d2f83a4af2408bbf8692526c
talhakhan54 <talhashahidkhan49@gmail.com>
2023-12-28 02:34:40 +0600

    __

--- remain/views.py
+++ remain/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Task
 # Create your views here.
 
 
 def home_page(request):
-    return render(request,'index.html')
+    task_list = Task.objects.all()
+    context = {
+        'task':task_list
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        disc = request.POST.get('disc')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        print(name,disc,date,time)
+        task = Task.objects.create(task_name=name,task_dis=disc,remainder_date=date,remainder_time=time)
+        task.save()
+        
+    
+    return render(request,'index.html',context)
+
+def dlt_task(request,id):
+    task = Task.objects.filter(id=id).first()
+    try:
+        task.delete()
+    except Exception as e:
+        return HttpResponse(e)
+    return redirect('/')
 
 
 
--- remainder/settings.py
+++ remainder/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+TIME_INPUT_FORMATS = ('%I:%M %p')
 # CELERY Settings 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
