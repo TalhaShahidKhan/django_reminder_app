2f1f28ceb42510d5e86b4fd59b2473c1818ef08b
talhakhan54 <talhashahidkhan49@gmail.com>
2024-01-01 09:18:26 +0600

    __

--- remain/views.py
+++ remain/views.py
@@ -15,8 +15,6 @@ def home_page(request):
         date = request.POST.get('date')
         time = request.POST.get('time')
         task = Task.objects.create(task_name=name,task_dis=disc,remainder_date=date,remainder_time=time)
-        result = add.delay(10,20)
-        print(result)
         task.save()
         
     
