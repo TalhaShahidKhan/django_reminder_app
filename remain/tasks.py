3120d2773d86a496e1d4431a432fa82b4e5b712d
talhakhan54 <talhashahidkhan49@gmail.com>
2024-01-04 09:49:10 +0600

    __

--- remain/tasks.py
+++ remain/tasks.py
@@ -1,11 +1,19 @@
 from celery import shared_task
 import time
 import datetime
+from plyer import notification
 
 @shared_task
-def remine(time):
-    rem_time = datetime.time.fromisoformat(time)
-    curr_time = datetime.datetime.now().strftime("%H:%M")
-    slp = rem_time - curr_time
-    print(slp)
+def remine(slp_time,name):
+    rem_time = datetime.time.fromisoformat(slp_time)
+    curr_time = datetime.datetime.now().time()
+    slp = datetime.datetime.combine(datetime.datetime.now(), rem_time) - datetime.datetime.combine(datetime.datetime.now(),curr_time)
+    time.sleep(int(str(round(slp.total_seconds()))))
+    notification.notify(
+        title = 'Django Reminder App',
+        message = name,
+        app_icon = None,
+        timeout = 5,
+    )
+
     
--- remain/views.py
+++ remain/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .models import Task
-import datetime
-import time
+from .tasks import remine
 # Create your views here.
-
 def home_page(request):
     task_list = Task.objects.all()
     context = {
@@ -16,11 +14,11 @@ def home_page(request):
         time = request.POST.get('time')
         task = Task.objects.create(task_name=name,task_dis=disc,remainder_time=time)
         task.save()
-        rem_time = datetime.time.fromisoformat(time)
-        curr_time = datetime.datetime.time.
-        print(type(rem_time), type(curr_time))
-        slp = datetime.datetime.combine(datetime.datetime.now(), rem_time) - datetime.datetime.combine(datetime.datetime.now(),curr_time)
-        print(slp)
+        remine.delay(time,name)
+        
+        
+        
+
         
         
         
